d32ff4822cbf8379d7c8ffe30a72e28a505be5d0
Tim <hello@timsmart.co>
2025-04-27 08:33:02 +1200

    change slider color

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -55,7 +55,7 @@ function Slider({
         <SliderPrimitive.Range
           data-slot="slider-range"
           className={cn(
-            "bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full",
+            "bg-[#db2a1c] absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full",
           )}
         />
       </SliderPrimitive.Track>
