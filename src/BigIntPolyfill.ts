c10538c9f5cdddc51f36b6ca086fe4b6588746aa
Tim Smart <hello@timsmart.co>
2026-04-19 21:07:17 +1200

    wip

--- src/BigIntPolyfill.ts
+++ src/BigIntPolyfill.ts
@@ -3,3 +3,9 @@ if (typeof globalThis.BigInt === "undefined") {
     return Math.floor(Number(input))
   }) as any
 }
+
+if (typeof (Object as any).hasOwn === "undefined") {
+  ;(Object as any).hasOwn = function (obj: any, prop: string) {
+    return Object.prototype.hasOwnProperty.call(obj, prop)
+  }
+}
