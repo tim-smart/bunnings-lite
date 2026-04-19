5ea5a3e028c9abd199a78f55d838d9eb2c0cfec4
Tim Smart <hello@timsmart.co>
2026-04-19 21:11:30 +1200

    wip

--- src/BigIntPolyfill.ts
+++ src/BigIntPolyfill.ts
@@ -9,3 +9,14 @@ if (typeof (Object as any).hasOwn === "undefined") {
     return Object.prototype.hasOwnProperty.call(obj, prop)
   }
 }
+
+if (typeof crypto.randomUUID === "undefined") {
+  crypto.randomUUID = function () {
+    const value = crypto.getRandomValues(new Uint8Array(16))
+    value[6] = (value[6] & 0x0f) | 0x40
+    value[8] = (value[8] & 0x3f) | 0x80
+    return Array.from(value)
+      .map((value) => value.toString(16).padStart(2, "0"))
+      .join("") as any
+  }
+}
